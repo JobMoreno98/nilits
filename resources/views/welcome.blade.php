6359dde86b2584a7ad358b5860317e491c448fed
Job Moreno <jobmoreno98@gmail.com>
2025-04-03 13:07:29 -0600

    cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,10 @@
 @section('title', 'Iniciar Sesión')
 
 @section('content')
-
     <div class="text-center mb-4">
         <img src="{{ asset('imgs/logo_NILITS23_color.png') }}" alt="Logo" style="width: 170px;">
     </div>
     <form method="POST" action="{{ route('login') }}">
-        <!-- Directiva Blade para el token CSRF -->
         @csrf
         <div class="form-group">
             <label for="inputUsuario">Usuario</label>
@@ -18,15 +16,11 @@
             <label for="inputPassword">Contraseña</label>
             <input type="password" id="pass" name="pass" class="form-control" placeholder="Contraseña" required>
         </div>
-        <button class="btn btn-lg text-light" style="background-color: rgb(0, 0, 169)" type="submit">Iniciar
+        <button class="btn btn-sm text-light" style="background-color: rgb(0, 0, 169)" type="submit">Iniciar
             Sesión</button>
     </form>
-    <a class="btn btn-lg  text-light" style="background-color: rgb(0, 0, 169)" href="{{ route('registro-usuario') }}">Darme
-        de
-        Alta</a>
-
-    {{--
-    <a class="btn btn-lg  text-light" style="background-color: rgb(0, 0, 169)" href="{{ route('alumnado') }}">Ver
-        Alumnado</a>
-        --}}
+    <a class="btn btn-sm text-light" style="background-color: rgb(0, 0, 169)" href="{{ route('registro-usuario') }}">Darme
+        de Alta Profesor</a>
+    <a class="btn btn-sm text-light" style="background-color: rgb(0, 0, 169)" href="{{ route('aspirantes.index') }}">Darme
+        de Alta Aspirante</a>
 @endsection
